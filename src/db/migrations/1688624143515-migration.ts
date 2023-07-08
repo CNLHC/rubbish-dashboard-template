01bfc5e774a81be7467a017ddf11e433f75061d1
Liu Hancheng <liuhancheng.cn@gmail.com>
2023-07-09 00:44:50 +0800

    feat: change init db migration to a general_cache table

--- src/db/migrations/1688624143515-migration.ts
+++ src/db/migrations/1688624143515-migration.ts
@@ -3,13 +3,20 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class Migration1688624143515 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     queryRunner.query(/*sql*/ `
-        SELECT 1
+      CREATE TABLE general_cache (
+          id          bigserial PRIMARY KEY,
+          cache_key   text NOT NULL,
+          cache_value jsonb NOT NULL DEFAULT '{}'::jsonb,
+          expires_at  timestamp with time zone NOT NULL DEFAULT now(),
+          CONSTRAINT unique_cache_key UNIQUE (cache_key)
+      );
+      CREATE INDEX general_cache_cache_key_uindex ON general_cache (cache_key, expires_at);
     `)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     queryRunner.query(/*sql*/ `
-        SELECT 1
+      DROP TABLE general_cache;
     `)
   }
 }
